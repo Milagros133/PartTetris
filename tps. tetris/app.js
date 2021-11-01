8e73ce01774864262996cc1c85ed8a1596d88c2a
Milagros133 <89795408+Milagros133@users.noreply.github.com>
2021-11-01 12:46:44 -0300

    Update app.js

--- tps. tetris/app.js
+++ tps. tetris/app.js
@@ -1,3 +1,4 @@
+// 45:37 a 46:10
 //undraw the Tetronino
 fuction undraw(){
     current.forEach(index=>{
@@ -5,9 +6,11 @@ fuction undraw(){
     })
 }
 
+//46:10 a 46:33
 //make the tetromino move down every second
 timerID = setInterval(moveDown, 1000)
 
+//56:40 a 59:20 (modico moveDown y moveRight)
 //assign functions to keyCodes
 function control(e) {
     if(e.keyCode === 37){
@@ -22,6 +25,7 @@ function control(e) {
 }
 document.addEventListener('keyup', control)
 
+//46:33 a 47:43 (agrego freeze() en el minuto 51:00)
 //move down function
 function moveDown() {
     undraw()
@@ -30,11 +34,12 @@ function moveDown() {
     freeze()
 }
 
+//47:43 a 50:20
 //freeze fuction
 function freeze() {
     if(current.some(index => squares[currentPosition + index + widthl].classList.contains('taken'))){
         current.forEach(index => squares[cureentPosition + index].classList.add('taken'))
-        //start a new tetromino falling
+        //start a new tetromino falling //50:20 a 51:00
         random = Math.floor(Math.random()) * theTretominoes.length)
         current = theTetrominoes[random][currentRotation]
         currentPosition = 4
@@ -42,6 +47,7 @@ function freeze() {
     }
 }
 
+//52:08 a 56:40
 //move the tetromino left, unless is at the edge or there is a blockage
 function moveLeft(){
     undraw()
